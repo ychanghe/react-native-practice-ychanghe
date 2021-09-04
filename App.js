a31b44217ff16e0d2c52386ceb334c314ad73bb6
ychanghe <yoon.chang3567@gmail.com>
2021-09-05 01:24:45 +0900

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ychanghe</Text>
+      <Text>
+        Nabaco is good.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
